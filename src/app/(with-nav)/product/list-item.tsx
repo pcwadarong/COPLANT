14863b2b4d0a4daef5c3bbc1816ccde099043b2c
Chaewon <cw06155@gmail.com>
2025-04-20 21:10:45 +0900

    able to connect detail, useCallBack

--- src/app/(with-nav)/product/list-item.tsx
+++ src/app/(with-nav)/product/list-item.tsx
@@ -1,11 +1,22 @@
 'use client';
 
+import Link from 'next/link';
 import Image from 'next/image';
 import { ProductPreview } from '@/types';
 
 export const ListItem = (item: ProductPreview) => (
-  <li key={item.id} className='flex flex-col items-center gap-4 font-bold'>
-    <Image src={item.imageUrls.list} alt={item.name} width={200} height={200} />
-    <p>{item.name}</p>
+  <li key={item.id}>
+    <Link
+      href={`product/${item.id}`}
+      className="flex flex-col items-center gap-4 font-bold"
+    >
+      <Image
+        src={item.imageUrls.list}
+        alt={item.name}
+        width={200}
+        height={200}
+      />
+      <p>{item.name}</p>
+    </Link>
   </li>
 );
