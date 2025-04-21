6ac632a5ff880b420c54d6a81f7cec3ac6998fe7
Chaewon <cw06155@gmail.com>
2025-04-22 02:31:08 +0900

    base64 admin-sdk.json

--- src/lib/firebase/firebaseAdminConfig.ts
+++ src/lib/firebase/firebaseAdminConfig.ts
@@ -1,10 +1,20 @@
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_ADMIN_KEY!, "base64").toString("utf-8")
+  );
+
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.applicationDefault(),
+//   });
+// }
 
 export const adminFirestore = admin.firestore();
 
